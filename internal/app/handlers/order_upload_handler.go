8a6520a2a526b29c1a0022ac0ef971671c1b7886
Nikita <>
2024-06-26 11:30:20 +0300

    tests were added

--- internal/app/handlers/order_upload_handler.go
+++ internal/app/handlers/order_upload_handler.go
@@ -10,12 +10,19 @@ import (
 
 func (h *Handler) OrderUploadHandler(w http.ResponseWriter, r *http.Request) {
 
+	//get request data
 	bodyBytes, err := io.ReadAll(r.Body)
 	if err != nil {
 		h.Logger.Errorf("error while reading body: %v", err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	if len(bodyBytes) == 0 {
+		h.Logger.Infof("empty request body")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	//todo: check with Luna`s alg
 	orderID := string(bodyBytes)
 	orderIDInt, err := strconv.Atoi(orderID)
@@ -25,6 +32,7 @@ func (h *Handler) OrderUploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//get userID
 	userID := r.Context().Value("userID")
 	if userID == nil {
 		h.Logger.Infof("user id wasn`t found in ctx")
@@ -38,6 +46,7 @@ func (h *Handler) OrderUploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//save new order
 	err = h.Storage.SaveNewOrder(userIDInt, orderIDInt, r.Context())
 	if errors.Is(err, gophermart_errors.MakeErrThisOrderWasUploadedByDifferentUser()) {
 		h.Logger.Infof("this order was already uploaded by different user")
@@ -49,6 +58,7 @@ func (h *Handler) OrderUploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//return
 	w.WriteHeader(http.StatusAccepted)
 	return
 }
