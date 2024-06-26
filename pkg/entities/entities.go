6abc6247561ebbb2a07e2a9189ff8fdf38e9005b
Nikita <>
2024-06-26 19:34:48 +0300

    time format fix

--- pkg/entities/entities.go
+++ pkg/entities/entities.go
@@ -1,6 +1,9 @@
 package entities
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type UserData struct {
 	ID       int    `json:"id"`
@@ -15,13 +18,34 @@ const (
 	OrderStatusProcessed  = "PROCESSED"
 )
 
+type TimeRFC3339 struct {
+	Time time.Time
+}
+
+func (t *TimeRFC3339) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.Time.Format(time.RFC3339))
+}
+
+func (t *TimeRFC3339) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+	parsedTime, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		return err
+	}
+	t.Time = parsedTime
+	return nil
+}
+
 type OrderData struct {
-	ID         int       `json:"-"`
-	UserID     int       `json:"-"`
-	Number     int       `json:"number"`
-	Status     string    `json:"status"`
-	Accural    int       `json:"accural"`
-	UploadedAt time.Time `json:"uploaded_at"`
+	ID         int         `json:"-"`
+	UserID     int         `json:"-"`
+	Number     int         `json:"number"`
+	Status     string      `json:"status"`
+	Accural    int         `json:"accural"`
+	UploadedAt TimeRFC3339 `json:"uploaded_at"`
 }
 
 // todo: хранить во флоате или в двух интах
